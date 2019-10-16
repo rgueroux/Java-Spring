077d2aad1d66296e2bc0d475ae59e60c5506244b
romain <romain.gueroux@imie.fr>
2019-10-16 21:43:04 +0200

    Premier commit

---
+++ src/main/java/com/example/tutorialthymeleaf/TutorialthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorialthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorialthymeleafApplication.class, args);
+    }
+
+}
